c9edef5594fc2559fc6a94eaed42c22381331827
kirkbushell <torm3nt@gmail.com>
2015-01-12 19:13:33 +0000

    PJAX router will now run on non-pjax requests, or fullpage requests.

--- pjax-router.js
+++ pjax-router.js
@@ -144,6 +144,25 @@ var Pjax = Pjax || {};
       requestCallback(xhr, options, 'after');
     });
 
+    /**
+     * When the page first loads up, PJAX will not be firing, so we want to make a match against
+     * the current document/window URL, and pass this information to the correct handler which
+     * should be able to handle both.
+     */
+    $(document).ready(function() {
+      var matchedRoutes = Pjax.Router.match(window.location.href, 'get', 'after');
+
+      for (var i = 0; i < matchedRoutes.length; i++) {
+        var handlerParams = {
+          xhr: null,
+          options: null,
+          route: matchedRoutes[i]
+        };
+
+        return handle(matchedRoutes[i].handler, handlerParams);
+      }
+    });
+
     // Return our object with the public methods
     return {
       listen: listen
